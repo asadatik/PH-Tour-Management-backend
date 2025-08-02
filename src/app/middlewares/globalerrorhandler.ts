05280f5d406783837d155e85e362da5e2c245a63
Asad Atik <asadatik1995@gmail.com>
2025-08-02 19:20:29 +0600

    Refactoring Error Handlers to separate functions

--- src/app/middlewares/globalerrorhandler.ts
+++ src/app/middlewares/globalerrorhandler.ts
@@ -3,80 +3,81 @@
 import { NextFunction, Request, Response } from "express"
 import AppError from "../errorHelper/appError"
 import { envVars } from "../config/env"
-
-
-
-
-
-
-
+import { TErrorSources } from "../interface/error.types"
+import { handlerDuplicateError } from "../helpers/handleDuplicateError"
+import { handleCastError } from "../helpers/handleCastError"
+import { handlerZodError } from "../helpers/handlerZodError"
+import { handlerValidationError } from "../helpers/handlerValidationError"
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
     
+        let errorSources: TErrorSources[] = []
     let statusCode = 500
     let message = "Something Went Wrong!!"
 
-
-       const err
-
- //Duplicate error
+    //Duplicate error
     if (err.code === 11000) {
-        statusCode = 400;
-    
-        message = `Duplicate field value entered: ${Object.keys(err.keyValue)[0]}` 
+        const simplifiedError = handlerDuplicateError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
     }
     // Object ID error / Cast Error
     else if (err.name === "CastError") {
-       
-        statusCode = 400;
-        message = `Invalid ${err.path}: ${err.value}`;
-
+        const simplifiedError = handleCastError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
     }
+    //Zod Error
 
-    // zod Validation Error
     else if (err.name === "ZodError") {
-        statusCode = 400;
-        message = 'zod Validation Error,,,,,,,,,,,,,,,,';
-
-        console.log( err.issues);
-
-   
+        const simplifiedError = handlerZodError(err)
+        statusCode = simplifiedError.statusCode
+        message = simplifiedError.message
+        errorSources = simplifiedError.errorSources as TErrorSources[]
     }
 
+    //Mongoose Validation Error
 
-
-
-    // Mongoose Validation Error
     else if (err.name === "ValidationError") {
-        statusCode = 400;
-        message = Object.values(err.errors).map((el: any) => el.message).join(", ");
+        const simplifiedError = handlerValidationError(err)
+        statusCode = simplifiedError.statusCode;
+        errorSources = simplifiedError.errorSources as TErrorSources[]
+        message = simplifiedError.message
     }
 
-    // JWT Error
-    else if (err.name === "JsonWebTokenError") {
-        statusCode = 401;
-        message = "Invalid Token";
-    } else if (err.name === "TokenExpiredError") {
-        statusCode = 401;
-        message = "Token Expired";
-    }
-
-      
-
+    //App Error
 
     else if (err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
-    } else if (err instanceof Error) {
+    }
+
+    //Unhandled Rejection
+    else if (err.name === "UnhandledRejection") {
+        statusCode = 500;
+        message = err.message || "Something went wrong"
+    }
+    //Unhandled Error
+    else if (err.name === "UnhandledError") {
+        statusCode = 500;
+        message = err.message || "Something went wrong"
+    }
+
+
+    //Other Errors
+     else if (err instanceof Error) {
         statusCode = 500;
         message = err.message
     }
 
-    res.status(statusCode).json({
+
+      res.status(statusCode).json({
         success: false,
         message,
-        err,
+        errorSources,
+        err: envVars.NODE_ENV === "development" ? err : null,
         stack: envVars.NODE_ENV === "development" ? err.stack : null
     })
+
 }
