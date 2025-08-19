d659e40418906ff395307892a61db06d5f1a03fb
Asad Atik <asadatik1995@gmail.com>
2025-08-19 18:51:25 +0600

    again back in this backend part

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -8,6 +8,8 @@ const authProviderSchema = new Schema<IAuthProvider>({
   {  versionKey: false,  _id: false
 })
 
+//                
+
 const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
